d8fc570abb34762b15c1aed4ca5e139505821457
sakshi kachate <sakshikachate347@gmail.com>
2026-01-27 17:45:51 +0530

    square of a number

---
+++ Assignments/Assignment9/program1_3.py
@@ -0,0 +1,15 @@
+# Function to square of given Number
+
+def Square(Value1):
+      Sqr = Value1 * Value1
+      return Sqr
+
+def main():
+    No1 = int(input("Enter a Number"))
+    Ret =Square(No1)
+    print(Ret)
+     
+        
+    
+if __name__ == "__main__":
+    main()
