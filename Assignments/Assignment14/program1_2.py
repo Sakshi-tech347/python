fbe43682d0e194aedd1d73ab58e2cb20e7b5aec7
sakshi kachate <sakshikachate347@gmail.com>
2026-01-28 19:31:09 +0530

    cube(no) using lambda

---
+++ Assignments/Assignment14/program1_2.py
@@ -0,0 +1,10 @@
+ Cube = lambda Value : Value* Value*Value
+
+def main():
+    
+        no1 = int(input("Enter First Number :"))
+        Ret =Cube(no1)
+        print(f"Cube of {no1} is :{Ret} ")
+  
+if __name__ == "__main__":
+    main()
