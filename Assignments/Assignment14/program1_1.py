4aa7a206c19f4ddbc34cc19a6f0c667b77efa507
sakshi kachate <sakshikachate347@gmail.com>
2026-01-28 19:30:03 +0530

    square(no)using lambda

---
+++ Assignments/Assignment14/program1_1.py
@@ -0,0 +1,12 @@
+
+Square = lambda Value :  Value * Value
+        
+
+
+def main():
+        no1 = int(input("Enter the Number  :"))
+        Ret = Square(no1)
+        print(f"Square of {no1} is :",Ret)
+
+if __name__ == "__main__":
+        main()
