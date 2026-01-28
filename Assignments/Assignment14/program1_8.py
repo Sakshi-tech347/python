00a1f884e0951d4fbd583563831d8703dfc3c871
sakshi kachate <sakshikachate347@gmail.com>
2026-01-28 20:06:41 +0530

    addition using lambda

---
+++ Assignments/Assignment14/program1_8.py
@@ -0,0 +1,14 @@
+ 
+Addition = lambda Value1,Value2 : Value1 + Value2
+
+
+def main():
+    
+        no1 = int(input("Enter First Number :"))
+        no2 = int(input("Enter Second Number :"))
+
+        Ret =Addition(no1,no2)
+        print(f"Addition is : {Ret}")
+               
+if __name__ == "__main__":
+    main()
