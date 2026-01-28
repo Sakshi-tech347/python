00874c8854e1d882124a9efa7231b6156934554e
sakshi kachate <sakshikachate347@gmail.com>
2026-01-28 20:03:38 +0530

    ChkEvn using lambda

---
+++ Assignments/Assignment14/program1_5.py
@@ -0,0 +1,13 @@
+ 
+Chck_Evn= lambda Value1 : (Value1 %2 == 0)
+
+
+def main():
+    
+        no1 = int(input("Enter First Number :"))
+
+        Ret =Chck_Evn(no1)
+        print(Ret)
+               
+if __name__ == "__main__":
+    main()
