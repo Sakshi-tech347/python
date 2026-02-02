9d335234dc3d59335ebe48eeed24c71a6057f21d
sakshi kachate <sakshikachate347@gmail.com>
2026-02-02 10:14:56 +0530

    Display 1 to N

---
+++ Assignments/Assignment12/program1_4.py
@@ -0,0 +1,17 @@
+ 
+
+def Print_From_1(Value1):
+     
+     for i in range(1 , Value1+1):
+         print(i ,end ="\t")
+
+
+def main():
+    No1 = int(input("Enter A First Number"))
+   
+    Print_From_1(No1)
+
+
+
+if __name__ == "__main__":
+    main()
