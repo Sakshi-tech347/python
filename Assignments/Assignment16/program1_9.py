da5a7371057054b53a0c80c7c7d8b8bb27c8d2c3
sakshi kachate <sakshikachate347@gmail.com>
2026-02-03 10:28:19 +0530

    display even numbers up to N

---
+++ Assignments/Assignment16/program1_9.py
@@ -0,0 +1,12 @@
+ def Display_Evn(Value):
+    for i in range(2,Value,2):
+        print(i)
+      
+def main():
+
+    No = int(input("Enter Number:"))
+    Display_Evn(No)
+      
+
+if __name__ == "__main__":
+    main()
