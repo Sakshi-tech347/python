7cc2aa7fe8149d42e0659e56a10029b6cc43da19
sakshi kachate <sakshikachate347@gmail.com>
2026-03-19 18:22:40 +0530

    module of file ModuleClient1.py

---
+++ Assignments/Assignment17/Arithmatic1.py
@@ -0,0 +1,17 @@
+# Q1 this is a module file 
+
+def add(Val1, Val2):
+    Ans = Val1 + Val2
+    return Ans
+
+def Sub(Val1, Val2):
+    Ans = Val1 - Val2
+    return Ans
+
+def Mult(Val1, Val2):
+    Ans = Val1 * Val2
+    return Ans
+
+def Div(Val1, Val2):
+    Ans = Val1 / Val2
+    return Ans
